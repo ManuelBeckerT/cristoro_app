04ffb0722ae76d6cb916c014b4087f24ba47b5e8
Manuel Becker <mabecker@uc.cl>
2018-07-19 13:19:42 -0400

    Register y Call funcionando bien

--- app/models/register.rb
+++ app/models/register.rb
@@ -1,3 +1,7 @@
 class Register < ApplicationRecord
   has_many :call
+
+  validates :correo, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :comuna, :calle, :numero, :encargado, :telefono, :correo, :contenedores, :establecimiento, :presence => true
+  validates :telefono, length: { minimum: 8 }
 end
