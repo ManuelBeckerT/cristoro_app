117deca103e77297baebbfeb17a412b1e2462b4e
Manuel Becker <mabecker@uc.cl>
2018-07-20 12:34:12 -0400

    Arreglos de form and register

--- app/models/register.rb
+++ app/models/register.rb
@@ -3,7 +3,8 @@ class Register < ApplicationRecord
 
   validates :correo, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :comuna, :calle, :numero, :encargado, :telefono, :correo, :contenedores, :establecimiento, :presence => true
-  validates :telefono, length: { minimum: 8 }
+  validates :telefono, length: { minimum: 7 }
+  validates :telefono, length: { maximum: 9 }
 
   validates_uniqueness_of :calle, scope: [:numero, :comuna], :case_sensitive => false
 
